896d51c7b879d4875c8402664117cbf09be4b577
anildalar <dollorinfotech@gmail.com>
2023-06-22 09:02:59 +0530

    new

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+
+//1. Define
+let myFunctionE = a => a*a ;
+
+
+//2. Call/ Invoke
+console.log(myFunctionE(2));
---
+++ js/custom2.js
@@ -0,0 +1,14 @@
+let students={
+    fname:"Abhishek",
+    lname:"Bairagi",
+    mobno:"097977998797987",
+    address:"Neemuch",
+    age:22,
+}
+
+// I want to extract some information from the student object
+
+let { fname } = students;
+
+console.log(fname)
+
